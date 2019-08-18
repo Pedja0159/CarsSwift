b93825dc8e3d8c2dc100af9d2fbfda8c88f92c29
pedja0159 <p.grujicic@yahoo.com>
2019-08-19 01:06:19 +0200

    dodata klasa AddCarVIewController

---
+++ AddCarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddCarViewController.swift
+//  Automobili2
+//
+//  Created by Pejo on 8/19/19.
+//  Copyright © 2019 pejo015. All rights reserved.
+//
+
+import UIKit
+
+class AddCarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
